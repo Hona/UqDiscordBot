e520e8dc65811834fc94ffe86ec498dba02e2c20
Luke Parker <10430890+TheRealHona@users.noreply.github.com>
2021-01-25 15:02:54 +1000

    Allow using in admin channel

--- src/UqDiscordBot.Discord/Commands/Checks/RequireBotChannelAttribute.cs
+++ src/UqDiscordBot.Discord/Commands/Checks/RequireBotChannelAttribute.cs
@@ -6,12 +6,13 @@ namespace UqDiscordBot.Discord.Commands.Checks
     public class RequireBotChannelAttribute : DescriptiveCheckBaseAttribute
     {
         private const ulong EnrolInCoursesChannelId = 803079176042446940;
+        private const ulong BotAdminChannelId = 803079434973216811;
         public RequireBotChannelAttribute()
         {
             FailureResponse = $"Please use the bot in the <#{EnrolInCoursesChannelId}> channel.";
         }
         
         public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help) 
-            => Task.FromResult(ctx.Channel.Id == EnrolInCoursesChannelId);
+            => Task.FromResult(ctx.Channel.Id == EnrolInCoursesChannelId || ctx.Channel.Id == BotAdminChannelId);
     }
 }
