7683804cd40acf03ed0475939d5a7603d122bf00
Luke Parker <lukemparker@outlook.com>
2021-02-22 13:26:21 +1000

    Add logging for bot

--- src/UqDiscordBot.Discord/Services/CommandService.cs
+++ src/UqDiscordBot.Discord/Services/CommandService.cs
@@ -18,8 +18,22 @@ namespace UqDiscordBot.Discord.Services
     [BotService(BotServiceType.InjectAndInitialize)]
     public class CommandService
     {
+        private DiscordChannel _discordChannel;
+        private DiscordClient _discordClient;
+        private async Task<DiscordChannel> GetLogChannelAsync()
+        {
+            if (_discordChannel != null)
+            {
+                return _discordChannel;
+            }
+
+            _discordChannel = await _discordClient.GetChannelAsync(803079434973216811);
+            return _discordChannel;
+        }
         public CommandService(IConfiguration config, DiscordClient discordClient, IServiceProvider services)
         {
+            _discordClient = discordClient;
+
             var commands = discordClient.UseCommandsNext(new CommandsNextConfiguration
             {
                 StringPrefixes = new[]
@@ -32,8 +46,8 @@ namespace UqDiscordBot.Discord.Services
                 Services = services
             });
 
-            discordClient.UseInteractivity(new InteractivityConfiguration() 
-            { 
+            discordClient.UseInteractivity(new InteractivityConfiguration()
+            {
                 PollBehaviour = PollBehaviour.KeepEmojis,
                 Timeout = TimeSpan.FromSeconds(60)
             });
@@ -47,6 +61,8 @@ namespace UqDiscordBot.Discord.Services
         {
             _ = Task.Run(async () =>
             {
+                var logChannel = await GetLogChannelAsync();
+
                 if (e.Exception is ChecksFailedException exception)
                 {
                     var embed = new DiscordEmbedBuilder
@@ -57,6 +73,11 @@ namespace UqDiscordBot.Discord.Services
                     };
 
                     await e.Context.RespondAsync(embed: embed);
+                    await logChannel.SendMessageAsync(exception.FailedChecks.ToCleanResponse(), embed: new DiscordEmbedBuilder
+                    {
+                        Title = "Exception",
+                        Description = exception.ToString()
+                    });
                 }
                 else if (e.Exception is UserException userException)
                 {
@@ -68,12 +89,17 @@ namespace UqDiscordBot.Discord.Services
                     };
 
                     await e.Context.RespondAsync(embed: embed);
+                    await logChannel.SendMessageAsync(embed: new DiscordEmbedBuilder
+                    {
+                        Title = "Exception",
+                        Description = userException.ToString()
+                    });
                 }
 
                 // No need to log when a command isn't found
                 else if (!(e.Exception is CommandNotFoundException))
                 {
-                    e.Context.Client.Logger.LogError(e.Exception, 
+                    e.Context.Client.Logger.LogError(e.Exception,
                         $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}",
                         DateTime.Now);
 
@@ -82,6 +108,12 @@ namespace UqDiscordBot.Discord.Services
                         Color = DiscordColor.Red,
                         Title = "Something went wrong",
                         Description = e.Exception.Message
+                    });
+
+                    await logChannel.SendMessageAsync(embed: new DiscordEmbedBuilder
+                    {
+                        Title = "Exception",
+                        Description = e.Exception.ToString()
                     });
                 }
             });
